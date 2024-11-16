fb7a3c379e45a7e362120d7f5c1ce1c67e7400fe
strudelsnfts <162649433+strudelsnfts@users.noreply.github.com>
2024-11-16 12:36:56 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { PublicKey } from '@solana/web3.js'
 import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'gamba-react-ui-v2'
 
 // Get RPC from the .env file or default to the public RPC.
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://mainnet.helius-rpc.com/?api-key=bd860cfe-6109-4edb-baa9-205561d2bbaa'
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://mainnet.helius-rpc.com/?api-key=d1e6e152-4a95-4e74-94c7-1bc7798187cb'
 
 // Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
